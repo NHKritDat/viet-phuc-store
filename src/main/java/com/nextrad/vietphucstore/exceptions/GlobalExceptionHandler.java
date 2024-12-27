1ff73ca32e2d0261ed31d751f891e5c40d601858
Krit Dat <dat01102003@gmail.com>
2024-12-27 14:47:27 +0700

    fix/exception: fix error message and handle NuillPointerException

--- src/main/java/com/nextrad/vietphucstore/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/nextrad/vietphucstore/exceptions/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.nextrad.vietphucstore.exceptions;
 
 import com.nextrad.vietphucstore.dtos.responses.standard.ApiItemResponse;
+import java.util.Arrays;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -18,5 +19,11 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ApiItemResponse<Boolean>> handleAuthorizationDeniedException(AuthorizationDeniedException e) {
         return ResponseEntity.status(403)
                 .body(new ApiItemResponse<>(e.getAuthorizationResult().isGranted(), e.getMessage()));
+    }
+
+    @ExceptionHandler(NullPointerException.class)
+    public ResponseEntity<ApiItemResponse<String>> handleNullPointerException(NullPointerException e) {
+        return ResponseEntity.status(500)
+                .body(new ApiItemResponse<>(Arrays.toString(e.getStackTrace()), e.getMessage()));
     }
 }
